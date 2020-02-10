f2f9cf77adc6ca407e4bc9468e3bc4e8f5008504
c18301026 <c18301026@mytudublin.ie>
2020-02-10 04:47:23 +0000

    Hello

---
+++ src/ie/tudublin/Main.java
@@ -0,0 +1,11 @@
+// javac -d bin src/ie/tudublin/*.java (compiles)
+// java -cp bin ie.tudublin.Main (runs)
+// cp = class path
+
+package ie.tudublin;
+
+public class Main {
+	public static void main(String[] args) {
+		System.out.println("Hello world");
+	}
+}
